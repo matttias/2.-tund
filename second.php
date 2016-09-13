25dbba3df68858774cfb9f0e4efc68c8ce5604f9
mattbleh <mattbleh@greeny.cs.tlu.ee>
2016-09-13 13:17:15 +0300

    if statement and for loop

---
+++ second.php
@@ -0,0 +1,22 @@
+<?php
+
+$age = 21; //komaga kirjutamisel peab kasutama punkti (.)
+
+if($age < 18) {
+	echo "alaealine";
+} else {
+	echo "täiskasvanu";
+}
+
+?>
+
+<br>
+
+<?php
+
+//eesmärk on vanusele vastavalt trükime välja sõna "palju"
+for ($i = 1; $i <= $age; $i++){
+	echo $i."Palju ";
+}
+echo "õnne!";
+?>
